a605e01862d8679b23a01da8de61e217516776c2
Mulualem03 <mulualemkahssay@gmail.com>
2025-03-23 11:27:17 -0700

    Refactor Hangman class with clearer naming and single-purpose methods

--- src/milestone_4.py
+++ src/milestone_4.py
@@ -1,42 +1,50 @@
 import random
 
 class Hangman:
-    def __init__(self, word_list, num_lives=5):
-        self.word_list = word_list
-        self.word = random.choice(word_list)
-        self.word_guessed = ["_" for _ in self.word]
-        self.num_letters = len(set(self.word))
-        self.num_lives = num_lives
-        self.list_of_guesses = []
+    def __init__(self, word_pool, num_lives=5):
+        self.word_pool = word_pool
+        self.target_word = random.choice(word_pool)
+        self.word_display = ["_" for _ in self.target_word]
+        self.remaining_unique_letters = len(set(self.target_word))
+        self.lives_left = num_lives
+        self.guessed_letters = []
+
+    def validate_guess(self, guess):
+        return len(guess) == 1 and guess.isalpha()
+
+    def has_been_guessed(self, guess):
+        return guess in self.guessed_letters
+
+    def reveal_letter_positions(self, guess):
+        for index, letter in enumerate(self.target_word):
+            if letter == guess:
+                self.word_display[index] = guess
 
     def check_guess(self, guess):
         guess = guess.lower()
-        if guess in self.word:
+        if guess in self.target_word:
             print(f"Good guess! {guess} is in the word.")
-            for index, letter in enumerate(self.word):
-                if letter == guess:
-                    self.word_guessed[index] = guess
-            self.num_letters -= 1
+            self.reveal_letter_positions(guess)
+            self.remaining_unique_letters -= 1
         else:
-            self.num_lives -= 1
+            self.lives_left -= 1
             print(f"Sorry, {guess} is not in the word.")
-            print(f"You have {self.num_lives} lives left.")
+            print(f"You have {self.lives_left} lives left.")
 
     def ask_for_input(self):
         while True:
             guess = input("Enter a single letter: ")
-            if len(guess) != 1 or not guess.isalpha():
+            if not self.validate_guess(guess):
                 print("Invalid letter. Please, enter a single alphabetical character.")
-            elif guess in self.list_of_guesses:
+            elif self.has_been_guessed(guess):
                 print("You already tried that letter!")
             else:
                 self.check_guess(guess)
-                self.list_of_guesses.append(guess)
+                self.guessed_letters.append(guess)
                 break
 
 game = Hangman(["apple", "banana", "cherry"])
 game.ask_for_input()
-print("Word progress:", game.word_guessed)
-print("Lives left:", game.num_lives)
-print("Letters remaining:", game.num_letters)
-
+print("Word progress:", game.word_display)
+print("Lives left:", game.lives_left)
+print("Letters remaining:", game.remaining_unique_letters)
